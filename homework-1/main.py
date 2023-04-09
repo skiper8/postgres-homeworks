914268357526bb8d85cf2a1fd1b284b38cee859a
askip <a.skiper2013@yandex.ru>
2023-04-09 14:52:50 +0300

    Добавление скрипта записи в БД

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,33 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+
+import psycopg2
+import csv
+
+with psycopg2.connect(host="localhost",
+                      database="north",
+                      user="postgres",
+                      password="456852") as conn:
+    with conn.cursor() as cur:
+        with open('north_data/customers_data.csv', 'r', encoding='UTF-8') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                cur.executemany("INSERT INTO customers VALUES (%s, %s, %s)",
+                                [(row['customer_id'], row['company_name'], row['contact_name'])])
+
+    with conn.cursor() as cur:
+        with open('north_data/orders_data.csv', 'r', encoding='UTF-8') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                cur.executemany("INSERT INTO orders VALUES (%s, %s, %s, %s, %s)",
+                                [(row['order_id'], row['customer_id'], row['employee_id'], row['order_date'],
+                                  row['ship_city'])])
+
+    with conn.cursor() as cur:
+        with open('north_data/employees_data.csv', 'r', encoding='UTF-8') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                cur.executemany("INSERT INTO employees VALUES (%s, %s, %s, %s, %s)",
+                                [(row['first_name'], row['last_name'], row['title'], row['birth_date'],
+                                  row['notes'])])
+
+conn.close()
